a6688be835fdc55ff79452104c0e618025fd3b42
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-04-11 11:58:11 +0200

    added variable colors

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script>
 
+  import TheHeader from "./components/TheHeader.vue";
+
+  export default {
+    components: {
+      TheHeader,
+    },
+    data() {
+      return {
+
+      }
+
+    }
+  }
 </script>
 
 <template>
-  
+  <TheHeader />
 </template>
 
 <style lang="scss">
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<script>
+
+
+</script>
+
+<template>
+<div class="container-fluid">
+    <div class="container"></div>
+</div>
+
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
