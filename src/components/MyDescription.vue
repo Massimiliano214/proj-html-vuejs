a1c2edcde11c564c022469b2f348317fa812daf1
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-04-12 10:32:46 +0200

    vehicle description ended

--- src/components/MyDescription.vue
+++ src/components/MyDescription.vue
@@ -5,25 +5,82 @@
 </script>
 
 <template>
-    <div class="container">
-        <h6>WHO WE ARE</h6>
-        <h2>
-            
-            <mark class="decorations"><i class="fa-regular fa-gem"></i> Excellence</mark>
-            in Transport
-        </h2>
+    <div class="container d-flex py-5">
+        <div class="containerLeft">
+            <h6>WHO WE ARE</h6>
+            <h2>
+                
+                <mark class="decorations"><i class="fa-regular fa-gem"></i> Excellence</mark>
+                in Transport
+            </h2>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita corrupti tempora voluptas qui omnis nobis suscipit voluptates architecto, mollitia molestias?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A blanditiis at doloremque quas aut perferendis praesentium odio commodi, hic earum?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita corrupti tempora voluptas qui omnis nobis suscipit voluptates architecto, mollitia molestias?</p>
+            <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. A blanditiis at doloremque quas aut perferendis praesentium odio commodi, hic earum?</p>
 
-        <em class="citation">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci neque nisi delectus eveniet magnam exercitationem assumenda obcaecati inventore similique architecto.</em>
+            <div class="citation">
+                <em>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci neque nisi delectus eveniet magnam exercitationem assumenda obcaecati inventore similique architecto.</em>
+
+            </div>
+
+            <p class="mt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates quisquam ea ratione aspernatur?</p>
+
+            <ul>
+                <li v-for="lorem in 5">Lorem ipsum dolor sit amet.</li>
+            </ul>
+        </div>
+
+        <div class="containerRight px-1">
+            <h4 class="pt-3">Vehicle types</h4>
+
+            <div class="veichleSector border-bottom py-3 d-flex">
+                <div class="veichleImg">
+                    <img class="veichle img-fluid" src="../assets/img/truck-1.png" alt="Mezzo di trasporto">
+
+                </div>
+                <div class="veichleDesc d-flex justify-content-center flex-column">
+                    <h3>Light</h3>
+                    <span>Max Weight 1200Kg</span>
+                </div>
+                
+            </div>
+
+            <div class="veichleSector border-bottom py-3 d-flex">
+                <div class="veichleImg">
+                    <img class="veichle img-fluid" src="../assets/img/truck-1.png" alt="Mezzo di trasporto">
+
+                </div>
+                <div class="veichleDesc d-flex justify-content-center flex-column">
+                    <h3>Medium</h3>
+                    <span>Max Weight 6000Kg</span>
+                </div>
+                
+            </div>
+
+            <div class="veichleSector border-bottom py-3 d-flex">
+                <div class="veichleImg">
+                    <img class="veichle img-fluid" src="../assets/img/truck-1.png" alt="Mezzo di trasporto">
+
+                </div>
+                <div class="veichleDesc d-flex justify-content-center flex-column">
+                    <h3>Heavy</h3>
+                    <span>Max Weight 24000Kg</span>
+                </div>
+                
+            </div>
+        </div>
     </div>
+        
 
 </template>
 
 <style scoped lang="scss">
     @import "../styles/general.scss";
 
+    .containerLeft {
+        color: $cube-darkGray;
+        width: 65%;
+        padding-right: 30px;
+    }
     h6 {
         padding-top: 20px;
         color: $cube-darkGreen;
@@ -31,14 +88,30 @@
     h2 {
         font-weight: bolder;
         padding: 20px 0;
+        color: black;
     }
     .citation {
         padding-left: 50px;
-        border-left: 5px solid $cube-darkGreen;
-    }
+        border-left: 5px solid ; 
+        border-color: rgba($color: $cube-darkGreen, $alpha: 0.2);
+        padding-top: 15px;
+        padding-bottom: 15px;
+    }   
     .decorations {
         color: $cube-darkGreen;
         background-color: rgba($color: $cube-darkGreen, $alpha: 0.2);
     }
 
+    .containerRight {
+        width: 35%;
+
+        .veichleImg {
+            padding-right: 20px;
+        }
+        .veichle {
+            background-color: $design-lightGreen;
+            width: 200px;
+            padding: 10px;
+        }
+    }
 </style>
