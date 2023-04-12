555ae8261475a7eacc7888b9a9a2c677ec4bff90
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-04-12 18:00:23 +0200

    finished bottomFooter

--- src/components/FooterBottom.vue
+++ src/components/FooterBottom.vue
@@ -5,12 +5,17 @@
 </script>
 
 <template>
-    <div class="container-fluid">
-        <div class="container">
-            <span>Enjoy the low price. We are tracking any intention of piracy.</span>
+    <div class="container-fluid py-4">
+        <div class="container d-flex justify-content-between">
+            <div class="divider">
+                <span>Enjoy the low price. We are tracking any intention of piracy.</span>
+            </div>
 
-            <i class="fa-regular fa-copyright"></i>
-            <span>2020 NEXGEN is Proudly Powered by Codings.</span>
+            <div class="divider">
+                <i class="px-2 fa-regular fa-copyright"></i>
+                <span>2020 NEXGEN is Proudly Powered by <span class="myColor">Codings</span>.</span>    
+            </div>
+            
         </div>
     </div>
 
@@ -19,4 +24,16 @@
 
 <style scoped lang="scss">
     @import "../styles/general.scss";
+
+    .container-fluid {
+        background-color: $cube-black;
+
+        .container {
+            color: $cube-lightGray;
+
+            .myColor {
+                color: $cube-lightGreen;
+            }
+        }
+    }
 </style>
